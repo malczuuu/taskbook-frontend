89d2811af6675027c72176070261689fe21c6bc0
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-06-14 20:19:54 +0200

    Update angular.json

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import '@angular/localize/init';
-import { enableProdMode } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowser().bootstrapModule(AppModule)
   .catch(err => console.error(err));
